3f27b38d47623eef73063e04ddd3df84bbc21f4f
James Earle <jaearle@microsoft.com>
2017-07-31 12:44:10 -0700

    opening two way communication between main and renderer with ipc

--- main.js
+++ main.js
@@ -58,3 +58,9 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+const { ipcMain } = require('electron')
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  event.sender.send('asynchronous-reply', 'pong')
+})
--- renderer.js
+++ renderer.js
@@ -1,9 +1,22 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-function openPrompt() {
-    const remote = require('electron').remote;
-    const BrowserWindow = remote.BrowserWindow;
+const { ipcRenderer } = require('electron')
+const remote = require('electron').remote;
+const path = require('path')
+const url = require('url')
 
+document.getElementById('prompt-button').addEventListener("click", () => {
+    const BrowserWindow = remote.BrowserWindow;
     var win = new BrowserWindow({ width: 400, height: 200 });
-}
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'prompt.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+});
+
+// Braces { } around object are a "destructuring assignment",
+// this is an ECMAScript 6 construct, feature of JavaScript 1.7
+// const ipcRenderer = require('electron').ipcRenderer;
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg) // prints "pong"
+})
+ipcRenderer.send('asynchronous-message', 'ping')
